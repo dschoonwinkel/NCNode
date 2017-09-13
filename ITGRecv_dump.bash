fa863604457dfc63bb4c3253ef1db04409b4b5b6
Daniel Schoonwinkel VBox Ubuntu <daniel.schoonwinkel@gmail.com>
2017-09-13 12:51:53 +0200

    Repaired ITGRecv_dump.bash to work with coding_topo1.py

--- ITGRecv_dump.bash
+++ ITGRecv_dump.bash
@@ -3,3 +3,4 @@
 #/home/daniel/Development/ITGTrafficGen/bin/ITGRecv -Sp 11234
 ipaddr=$(ip addr show | grep eth | grep inet | awk -F ' ' '{print $2}' | sed 's/\(.*\)../\1/')
 echo $ipaddr
+ITGRecvDump -D data_files/receiverdump_$ipaddr.bin -Sp 11234
